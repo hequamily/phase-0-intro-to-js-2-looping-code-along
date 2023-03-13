84b0afedb02371f8a992ca496a33c52a21c847f5
Harrison Quamily <quamilyh@gmail.com>
2023-03-13 10:35:26 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+// const names = ['Alice', 'Bob', 'Charlie'];
+// const occasion = 'birthday';
+// const messages = writeCards(names, occasion);
+
+
+
+// function writeCards(names, occasion) {
+//     const messages = [];
+  
+//     for (let i = 0; i < names.length; i++) {
+//       messages.push(`Thank you, ${names[i]}, for the ${occasion} gift!`);
+//     }
+  
+//     return messages;
+//   }
+//   console.log(messages);
+
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+
+
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
